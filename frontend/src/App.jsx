45fb5ba31e9186681a9049d66b733fcfe34ea365
Ankit Kharel <ankeetkharel@gmail.com>
2025-05-09 18:47:54 +0545

    NavBar complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,21 +9,22 @@ import Cart from './pages/Collection'
 import Login from './pages/Collection'
 import PlaceOrder from './pages/Collection'
 import Orders from './pages/Collection'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/collection' element={<Collection/>} />
-        <Route path='/about' element={<About />}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/product/:productId' element={<Product/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<Login/>} />
-        <Route path='/place-order' element={< PlaceOrder/>}/>
-        <Route path='orders' element={<Orders/>}/>
-
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={< PlaceOrder />} />
+        <Route path='orders' element={<Orders />} />
       </Routes>
     </div>
   )
