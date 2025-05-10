f5257cb45d2c71b1a253f72ac2ea305055b6f9e3
Ankit Kharel <ankeetkharel@gmail.com>
2025-05-10 10:03:25 +0545

    Latest Collection Component Complete

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const LatestCollection = () => {
+    const { products } = useContext(ShopContext);
+    // its like opening a box to get products from inside the box   
+    const [latestProducts, setLatestProducts] = useState([]);
+    useEffect(()=>{
+        setLatestProducts(products.slice(0, 10))
+    }, []);
+    return (
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={'LATEST'} text2={'COLLECTION'} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                    Discover our newest arrivals, carefully curated to match the latest trends and styles.
+                    Whether you're refreshing your wardrobe or looking for something bold and unique, we've got you covered.
+                    Each piece in this collection blends modern design with comfort and quality.
+                </p>
+            </div>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+                {
+                    latestProducts.map((product, index)=>(
+                        <ProductItem key={index} id={product._id} image={product.image} name={product.name} price={product.price}/>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default LatestCollection
---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,22 @@
+import { createContext } from 'react';
+import {products} from '../assets/frontend_assets/assets'
+
+export const ShopContext = createContext();
+// Think of ShopContext like a special box where you can put important data (like the shopping cart, user info, or product list), 
+// and then let any component in your app open that box and use the data— without passing it down manually through props.
+
+const ShopContextProvider = (props)=>{
+    const currency = '$';
+    const deliverFee = 100;
+    const value = {
+        products, deliverFee,currency
+    }
+    return (
+        // fills the box with data and lets other components use it.
+        <ShopContext.Provider value={value}> 
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
