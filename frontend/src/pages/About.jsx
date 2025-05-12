73ca98dd7bb35555667ce12a3ef57276602e7f7e
Ankit Kharel <ankeetkharel@gmail.com>
2025-05-12 18:36:18 +0545

    Product page complete

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
 
 export const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div></div>;
+};
+export default About;
