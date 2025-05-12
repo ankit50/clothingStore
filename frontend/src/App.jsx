d356b896da73329f6deda359810fbcf64f344228
Ankit Kharel <ankeetkharel@gmail.com>
2025-05-12 14:50:52 +0545

    Search Feature Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/Collection'
-import Contact from './pages/Collection'
-import Product from './pages/Collection'
-import Cart from './pages/Collection'
-import Login from './pages/Collection'
-import PlaceOrder from './pages/Collection'
-import Orders from './pages/Collection'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/Collection";
+import Contact from "./pages/Collection";
+import Product from "./pages/Collection";
+import Cart from "./pages/Collection";
+import Login from "./pages/Collection";
+import PlaceOrder from "./pages/Collection";
+import Orders from "./pages/Collection";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
+      <SearchBar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/collection' element={<Collection />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/product/:productId' element={<Product />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/place-order' element={< PlaceOrder />} />
-        <Route path='orders' element={<Orders />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="orders" element={<Orders />} />
       </Routes>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,22 +1,28 @@
-import { createContext } from 'react';
-import {products} from '../assets/frontend_assets/assets'
+import { createContext, useState } from "react";
+import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext();
-// Think of ShopContext like a special box where you can put important data (like the shopping cart, user info, or product list), 
+// Think of ShopContext like a special box where you can put important data (like the shopping cart, user info, or product list),
 // and then let any component in your app open that box and use the data— without passing it down manually through props.
 
-const ShopContextProvider = (props)=>{
-    const currency = '$';
-    const deliverFee = 100;
-    const value = {
-        products, deliverFee,currency
-    }
-    return (
-        // fills the box with data and lets other components use it.
-        <ShopContext.Provider value={value}> 
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+const ShopContextProvider = (props) => {
+  const currency = "$";
+  const deliverFee = 100;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const value = {
+    products,
+    deliverFee,
+    currency,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
+  return (
+    // fills the box with data and lets other components use it.
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
