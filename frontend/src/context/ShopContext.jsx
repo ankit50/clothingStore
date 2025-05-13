ebb3eef11f8442914edf5bfbdca35b86cdf0e724
Ankit Kharel <ankeetkharel@gmail.com>
2025-05-13 14:32:47 +0545

    Cart count complete

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,15 +1,44 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 // Think of ShopContext like a special box where you can put important data (like the shopping cart, user info, or product list),
 // and then let any component in your app open that box and use the data— without passing it down manually through props.
 
 const ShopContextProvider = (props) => {
-  const currency = "$";
+  const currency = "NPR ";
   const deliverFee = 100;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+  const addToCart = async (productId, size) => {
+    if (!size) {
+      size = "M";
+    }
+    let cartData = structuredClone(cartItems);
+    if (cartData[productId]) {
+      if (cartData[productId][size]) {
+        cartData[productId][size] += 1;
+      } else {
+        cartData[productId][size] = 1;
+      }
+    } else {
+      cartData[productId] = {};
+      cartData[productId][size] = 1;
+    }
+    setCartItems(cartData);
+  };
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const eachProduct in cartItems) {
+      const product = cartItems[eachProduct];
+      for (const size in product) {
+        totalCount = totalCount + product[size];
+      }
+    }
+    return totalCount;
+  };
   const value = {
     products,
     deliverFee,
@@ -18,7 +47,11 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItems,
+    addToCart,
+    getCartCount,
   };
+
   return (
     // fills the box with data and lets other components use it.
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
