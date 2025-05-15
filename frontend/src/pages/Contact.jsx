d7c19435f73580d7893ed820cb58536f7c28350c
Ankit Kharel <ankeetkharel@gmail.com>
2025-05-15 10:09:09 +0545

    Contact us page complete

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import NewsLetterBox from "../components/NewsLetterBox";
+import { assets } from "../assets/frontend_assets/assets";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT "} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt="contact us"
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            Koteshwor-32, Nikhileshwor
+            <br />
+            Bagmati, Kathmandu
+          </p>
+          <p className="text-gray-500">
+            Tel:0023-5603232 <br />
+            Email:forver@gmail.com
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p className="text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
