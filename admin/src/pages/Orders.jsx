c6b205f5c5b9e9074e401e4f2e0421d9e71b3192
Ankit Kharel <ankeetkharel@gmail.com>
2025-05-21 12:52:38 +0545

    Order status update by admin complete

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
-import { backendUrl } from "../App";
+import { backendUrl, currency } from "../App";
 import { toast } from "react-toastify";
 import { assets } from "../assets/assets";
 
@@ -28,6 +28,26 @@ const Orders = ({ token }) => {
       toast.error(error.message);
     }
   };
+
+  const statusHandler = async (e, orderId) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/status",
+        {
+          orderId,
+          status: e.target.value,
+        },
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        await fetchALlOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message);
+    }
+  };
+
   useEffect(() => {
     fetchALlOrders();
   }, [token]);
@@ -36,20 +56,23 @@ const Orders = ({ token }) => {
       <h3>Order Page</h3>
       <div>
         {orders.map((order, index) => (
-          <div key={index}>
-            <img src={assets.parcel_icon} alt="" />
+          <div
+            className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700"
+            key={index}
+          >
+            <img className="w-12" src={assets.parcel_icon} alt="" />
             <div>
               <div>
                 {order.items.map((item, index) => {
                   if (index === order.items.length - 1) {
                     return (
-                      <p key={index}>
+                      <p className="py-0.5" key={index}>
                         {item.name} x {item.quantity} <span>{item.size}</span>
                       </p>
                     );
                   } else {
                     return (
-                      <p key={index}>
+                      <p className="py-0.5" key={index}>
                         {item.name} x {item.quantity} <span>{item.size}</span>,
                       </p>
                     );
@@ -57,7 +80,9 @@ const Orders = ({ token }) => {
                 })}
               </div>
 
-              <p>{order.address.firstName + " " + order.address.lastName}</p>
+              <p className="mt-3 mb-2 font-medium">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
               <div>
                 <p>{order.address.street + " ,"}</p>
                 <p>
@@ -72,6 +97,28 @@ const Orders = ({ token }) => {
               </div>
               <p>{order.address.phone}</p>
             </div>
+            <div>
+              <p className="text-sm sm:text-[15px]">
+                items: {order.items.length}
+              </p>
+              <p>Method: {order.paymentMethod}</p>
+              <p>Payment: {order.payment ? "Done" : "Pending"}</p>
+              <p>Date: {new Date(order.date).toDateString()}</p>
+            </div>
+            <p>
+              {currency} {order.amount}
+            </p>
+            <select
+              onChange={(e) => statusHandler(e, order._id)}
+              value={order.status}
+              className="p-2 font-semibold"
+            >
+              <option value="Order Placed">Order Placed</option>
+              <option value="Packing">Packing</option>
+              <option value="Shipped">Shipped</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
           </div>
         ))}
       </div>
